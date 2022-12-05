958c1e1b0760ff13d2422c365ad3182d352a073b
Warspiteful <jalmendral24@gmail.com>
2022-12-05 13:14:10 -0500

    Incorporated GetPossibleActions into Game

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 from hand import hand
 from state import state
+from itertools import combinations
 
 import Tier1Deck 
 import Tier2Deck 
@@ -76,6 +77,97 @@ class game:
                 if gameState.getPlayerhand.getPrestige >= 15 or \
                         gameState.getComputerHand.getPrestige >= 15:
                         self.gameOver = True
+
+        TOKEN_TYPES = ['green', 'blue', 'red', 'white', 'black', 'yellow']
+        POSSIBLE_ACTIONS = ['take_3', 'take_2', 'reserve', 'purchase', 'do_nothing']
+        JOKER_COLOR = 'yellow'
+        MIN_TOKEN_FOR_TAKE_2 = 4
+        BOARD_X, BOARD_Y = (3, 4)
+        MAX_RESERVED_CARDS = 3
+
+        def get_subsets(li, size=3):
+                return set(combinations(li, size))
+
+        def get_possible_actions(self, gamestate, player):
+                actions = []
+
+                # First type : take_3
+                take_3 = self.POSSIBLE_ACTIONS[0]
+                # Retrieve list of tokens minus the yellow ones (they cannot be picked)
+                allowed_tokens = self.TOKEN_TYPES.copy()
+                allowed_tokens.remove(self.JOKER_COLOR)
+                available_tokens = set()
+                # Check if there's still available tokens of each color
+                for color in allowed_tokens:
+                        if self.state.still_has_token(color):
+                                available_tokens.add(color)
+                # Get all 3-tuples of available tokens
+                all_token_tuples = self.get_subsets(available_tokens, 3)
+                # Add them to the list of possible actions
+                for token_tuple in all_token_tuples:
+                        new_action = {
+                        'type': take_3,
+                        'params': token_tuple
+                        }
+                        actions.append(new_action)
+
+                # Second type : take_2
+                take_2 = self.POSSIBLE_ACTIONS[1]
+                for color in allowed_tokens:
+                        if gamestate.tokens[color] >= self.MIN_TOKEN_FOR_TAKE_2:
+                                new_action = {
+                                        'type': take_2,
+                                        'params': color
+                                }
+                                actions.append(new_action)
+
+                # Third type : reserve
+                reserve = self.POSSIBLE_ACTIONS[2]
+                if len(player.hand) < self.MAX_RESERVED_CARDS:
+                        # Pick a reserved cards from the middle of the table
+                        for i in range(self.BOARD_X):
+                                for j in range(self.game.BOARD_Y):
+                                        if gamestate.cards[i][j].is_empty():
+                                                break
+                                        new_action = {
+                                        'type': reserve,
+                                        'params': ['from_table', (i, j)]
+                                        }
+                                        actions.append(new_action)
+
+                # Fourth : buy a card
+                purchase = self.POSSIBLE_ACTIONS[3]
+                for i in range(self.BOARD_X):
+                        for j in range(self.BOARD_Y):
+                                if gamestate.cards[i][j].is_empty():
+                                        break
+                                card = gamestate.cards[i][j]
+                                if player.can_buy(card):
+                                        new_action = {
+                                        'type': purchase,
+                                        'params': ['from_table', (i, j)]
+                                        }
+                                        actions.append(new_action)
+                for i in range(len(player.hand)):
+                        card = player.hand[i]
+                        if player.can_buy(card):
+                                new_action = {
+                                        'type': purchase,
+                                        'params': ['from_hand', i]
+                                }
+                                actions.append(new_action)
+                                
+                # Else: do nothing
+                if len(actions) == 0:
+                        do_nothing = self.POSSIBLE_ACTIONS[4]
+                        new_action = {
+                        'type': do_nothing,
+                        'params': None
+                        }
+                        actions.append(new_action)
+                        
+
+                return(actions)
 
         def getWinLoss(self):
                 ans = 0
