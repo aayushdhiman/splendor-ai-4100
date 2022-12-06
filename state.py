c6b4c3e2db3ddfcc04bee27a4e1efbe540c68b69
Warspiteful <jalmendral24@gmail.com>
2022-12-05 20:15:16 -0500

    Moved action functionality from game to state

--- state.py
+++ state.py
@@ -1,6 +1,13 @@
 from hand import hand 
+from itertools import combinations
 
 class state:
+        TOKEN_TYPES = ['green', 'blue', 'red', 'white', 'black', 'yellow']
+        POSSIBLE_ACTIONS = ['take_3', 'take_2', 'reserve', 'purchase', 'do_nothing']
+        JOKER_COLOR = 'yellow'
+        MIN_TOKEN_FOR_TAKE_2 = 4
+        BOARD_X, BOARD_Y = (3, 4)
+        MAX_RESERVED_CARDS = 3
 
         def __init__(self, pool : dict, table : list, deck1 : list, deck2 : list, deck3 : list, playerHand : hand, computerHand : hand):
                 """
@@ -97,6 +104,112 @@ class state:
                 if self.getPlayerHand().getPrestige() >= 15:
                         ans = -1
                 return ans
+
+        def isOver(self):
+                return self.getPlayerhand.getPrestige >= 15 or \
+                        self.getComputerHand.getPrestige >= 15
+       
+
+
+        def get_subsets(self, li, size=3):
+                return set(combinations(li, size))
+
+        def get_possible_actions(self):
+                actions = []
+                if(self.turns):
+                        player : hand = self.playerHand
+                else:
+                        player : hand = self.computerHand
+
+                # First type : take_3
+                take_3 = self.POSSIBLE_ACTIONS[0]
+                # Retrieve list of tokens minus the yellow ones (they cannot be picked)
+                allowed_tokens = self.TOKEN_TYPES.copy()
+                allowed_tokens.remove(self.JOKER_COLOR)
+                available_tokens = set()
+                # Check if there's still available tokens of each color
+                for color in allowed_tokens:
+                        if self.still_has_token(color):
+                                available_tokens.add(color)
+                # Get all 3-tuples of available tokens
+                all_token_tuples = self.get_subsets(available_tokens, 3)
+                # Add them to the list of possible actions
+                for token_tuple in all_token_tuples:
+                        new_action = {
+                        'type': take_3,
+                        'params': token_tuple
+                        }
+                        actions.append(new_action)
+
+                # Second type : take_2
+                take_2 = self.POSSIBLE_ACTIONS[1]
+                for color in allowed_tokens:
+                        if self.pool[color] >= self.MIN_TOKEN_FOR_TAKE_2:
+                                new_action = {
+                                        'type': take_2,
+                                        'params': color
+                                }
+                                actions.append(new_action)
+
+                # Third type : reserve
+                reserve = self.POSSIBLE_ACTIONS[2]
+                if len(player.getDeck()) < self.MAX_RESERVED_CARDS:
+                        # Pick a reserved cards from the middle of the table
+                        for i in range(self.BOARD_X):
+                                for j in range(self.BOARD_Y):
+                                        if self.GetCardAtTableLocation([i,j]) != None:
+                                                break
+                                        new_action = {
+                                        'type': reserve,
+                                        'params': ['from_table', (i, j)]
+                                        }
+                                        actions.append(new_action)
+
+                # Fourth : buy a card
+                purchase = self.POSSIBLE_ACTIONS[3]
+                for i in range(self.BOARD_X):
+                        for j in range(self.BOARD_Y):
+                                if self.GetCardAtTableLocation([i,j]) != None:
+                                        break
+                                card = self.GetCardAtTableLocation(i,j)
+                                if player.CanBuy(card):
+                                        new_action = {
+                                        'type': purchase,
+                                        'params': ['from_table', (i, j)]
+                                        }
+                                        actions.append(new_action)
+                for i in range(len(player.deck)):
+                        card = player.deck[i]
+                        if player.CanBuy(card):
+                                new_action = {
+                                        'type': purchase,
+                                        'params': ['from_hand', i]
+                                }
+                                actions.append(new_action)
+                                
+                # Else: do nothing
+                if len(actions) == 0:
+                        do_nothing = self.POSSIBLE_ACTIONS[4]
+                        new_action = {
+                        'type': do_nothing,
+                        'params': None
+                        }
+                        actions.append(new_action)
+                        
+
+                return(actions)
+
+        def getSuccessors(self):
+                successor = []
+                for action in self.get_possible_actions():
+                        successor.append([action, self.ParseAction(action)])
+
+                return successor
+
+        def getSuccessor(self, action):
+                return [action, self.ParseAction(action)]
+
+
 
         
         def __repr__(self):
